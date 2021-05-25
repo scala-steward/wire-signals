dad35476a3bcc2741920814c899d7454a32c7393
Maciej Gorywoda <makingthematrix@gmail.com>
2021-05-25 21:50:55 +0200

    switch from withDelay to waitForResult

--- core/src/test/scala/com/wire/signals/AggregatingSignalSpec.scala
+++ core/src/test/scala/com/wire/signals/AggregatingSignalSpec.scala
@@ -52,38 +52,34 @@ class AggregatingSignalSpec extends munit.FunSuite {
       (b, a) => b :+ a.length
     )
 
-    var value = Seq.empty[Int]
+    val value = Signal(Seq.empty[Int])
 
-    as.foreach { value = _ }
-    assertEquals(value, Seq.empty)
+    as.foreach { value ! _ }
+    waitForResult(value, Seq.empty)
     assertEquals(as.value, None)
 
     promise.success(Seq(42))
 
-    withDelay {
-      assertEquals(value, Seq(42))
-      assertEquals(result(as.future), Seq(42))
-    }
+    waitForResult(value, Seq(42))
+    waitForResult(as, Seq(42))
+
 
     updater ! "meep"
 
-    withDelay {
-      assertEquals(value, Seq(42, 4))
-      assertEquals(result(as.future), Seq(42, 4))
-    }
+    waitForResult(value, Seq(42, 4))
+    waitForResult(as, Seq(42, 4))
 
     as.unsubscribeAll()
 
     updater ! "yay"
+    awaitAllTasks
 
-    withDelay {
-      assertEquals(value, Seq(42, 4))
-      assertEquals(result(as.future), Seq(42, 4))
-    }
+    waitForResult(value, Seq(42, 4))
+    waitForResult(as, Seq(42, 4))
   }
 
   test("events while subscribed but still loading") {
-    var promise = Promise[Seq[Int]]()
+    val promise = Promise[Seq[Int]]()
     def loader() = promise.future
     val updater = EventStream[String]()
 
@@ -93,29 +89,25 @@ class AggregatingSignalSpec extends munit.FunSuite {
       (b, a) => b :+ a.length
     )
 
-    var value = Seq.empty[Int]
+    val value = Signal(Seq.empty[Int])
 
-    as.foreach { value = _ }
-    assertEquals(value, Seq.empty)
+    as.foreach { value ! _ }
+    waitForResult(value, Seq.empty)
     assertEquals(as.value, None)
 
     updater ! "meep"
     updater ! "moop"
     updater ! "eek"
 
-    withDelay {
-      assertEquals(value, Seq.empty)
-      assertEquals(as.value, None)
-    }
+    waitForResult(value, Seq.empty)
+    assertEquals(as.value, None)
 
     updater ! "!"
     promise.success(Seq(42))
     updater ! "supercalifragilisticexpialidocious"
 
-    withDelay {
-      assertEquals(value, Seq(42, 4, 4, 3, 1, 34))
-      assertEquals(result(as.future), Seq(42, 4, 4, 3, 1, 34))
-    }
+    waitForResult(value, Seq(42, 4, 4, 3, 1, 34))
+    waitForResult(as, Seq(42, 4, 4, 3, 1, 34))
   }
 
   test("reload on re-wire"){
@@ -129,65 +121,53 @@ class AggregatingSignalSpec extends munit.FunSuite {
       (b, a) => b :+ a.length
     )
 
-    var value = Seq.empty[Int]
+    val value = Signal(Seq.empty[Int])
 
-    as.foreach { value = _ }
+    as.foreach { value ! _ }
     promise.success(Seq(42))
 
     updater ! "wow"
     updater ! "such"
     updater ! "publish"
 
-    withDelay {
-      assertEquals(value, Seq(42, 3, 4, 7))
-      assertEquals(result(as.future), Seq(42, 3, 4, 7))
-    }
+    waitForResult(value, Seq(42, 3, 4, 7))
+    waitForResult(as, Seq(42, 3, 4, 7))
 
     as.unsubscribeAll()
-
-    withDelay {
-      // still holds to the last computed value after unsubscribing
-      assertEquals(value, Seq(42, 3, 4, 7))
-      assertEquals(result(as.future), Seq(42, 3, 4, 7))
-    }
+    awaitAllTasks
+    // still holds to the last computed value after unsubscribing
+    waitForResult(value, Seq(42, 3, 4, 7))
+    waitForResult(as, Seq(42, 3, 4, 7))
     // triggers reload
     updater ! "publisher"
+    awaitAllTasks
 
-    withDelay {
-      // still the old value
-      assertEquals(value, Seq(42, 3, 4, 7))
-      // a new value after reload
-      assertEquals(result(as.future), Seq(42, 9))
-    }
+    // still the old value
+    waitForResult(value, Seq(42, 3, 4, 7))
+    // a new value after reload
+    waitForResult(as, Seq(42, 9))
 
     promise = Promise[Seq[Int]]()
-    as.foreach { value = _ }
+    as.foreach { value ! _ }
 
-    withDelay {
-      assertEquals(value, Seq(42, 3, 4, 7))
-      assertEquals(result(as.future), Seq(42, 9))
-    }
+    awaitAllTasks
+    waitForResult(value, Seq(42, 3, 4, 7))
+    waitForResult(as, Seq(42, 9))
 
     updater ! "much amaze"
 
-    withDelay {
-      assertEquals(value, Seq(42, 9, 10))
-      assertEquals(result(as.future), Seq(42, 9, 10))
-    }
+    waitForResult(value, Seq(42, 9, 10))
+    waitForResult(as, Seq(42, 9, 10))
 
     promise.success(Seq(42, 3, 4, 7, 9))
 
-    withDelay {
-      assertEquals(value, Seq(42, 3, 4, 7, 9, 10))
-      assertEquals(result(as.future), Seq(42, 3, 4, 7, 9, 10))
-    }
+    waitForResult(value, Seq(42, 3, 4, 7, 9, 10))
+    waitForResult(as, Seq(42, 3, 4, 7, 9, 10))
 
     updater ! "much"
     updater ! "amaze"
 
-    withDelay {
-      assertEquals(value, Seq(42, 3, 4, 7, 9, 10, 4, 5))
-      assertEquals(result(as.future), Seq(42, 3, 4, 7, 9, 10, 4, 5))
-    }
+    waitForResult(value, Seq(42, 3, 4, 7, 9, 10, 4, 5))
+    waitForResult(as, Seq(42, 3, 4, 7, 9, 10, 4, 5))
   }
 }
--- core/src/test/scala/com/wire/signals/testutils/package.scala
+++ core/src/test/scala/com/wire/signals/testutils/package.scala
@@ -15,32 +15,8 @@ package object testutils {
 
   def random: Random = localRandom.get
 
-  implicit class SignalToSink[A](val signal: Signal[A]) extends AnyVal {
-    def sink: SignalSink[A] = returning(new SignalSink[A])(_.subscribe(signal)(EventContext.Global))
-  }
-
-  class SignalSink[A] {
-    @volatile private var sub = Option.empty[Subscription]
-
-    def subscribe(s: Signal[A])(implicit ctx: EventContext = EventContext.Global): Unit = sub = Some(s.onCurrent(v => value = Some(v)))
-
-    def unsubscribe(): Unit = sub.foreach { s =>
-      s.destroy()
-      sub = None
-    }
-
-    @volatile private[testutils] var value = Option.empty[A]
-
-    def current: Option[A] = value
-  }
-
   implicit class EnrichedInt(val a: Int) extends AnyVal {
     def times(f: => Unit): Unit = (1 to a).foreach(_ => f)
-  }
-
-  implicit class RichOption[A](val opt: Option[A]) extends AnyVal {
-    @inline final def fold2[B](ifEmpty: => B, f: A => B): B = if (opt.isEmpty) ifEmpty else f(opt.get)
-    // option's catamorphism with better type inference properties than the one provided by the std lib
   }
 
   @tailrec
