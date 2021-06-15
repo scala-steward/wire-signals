09a552033e697bc9ac669d6823aedfc8c552defc
Maciej Gorywoda <makingthematrix@gmail.com>
2021-06-15 18:03:34 +0200

    reintroducing a test 'reload on re-wire'

--- core/src/test/scala/com/wire/signals/AggregatingSignalSpec.scala
+++ core/src/test/scala/com/wire/signals/AggregatingSignalSpec.scala
@@ -109,4 +109,78 @@ class AggregatingSignalSpec extends munit.FunSuite {
     waitForResult(value, Seq(42, 4, 4, 3, 1, 34))
     waitForResult(as, Seq(42, 4, 4, 3, 1, 34))
   }
+
+  // this test is a special case - we don't want to use `waitForResult` here because it wires the signal and we want to keep it un-wired on purpose
+  test("reload on re-wire"){
+    var promise = Promise[Seq[Int]]()
+    def loader() = promise.future
+    val updater = EventStream[String]()
+
+    val as = new AggregatingSignal[String, Seq[Int]](
+      loader _,
+      updater,
+      (b, a) => b :+ a.length
+    )
+
+    var value = Seq.empty[Int]
+
+    as.foreach { value = _ }
+    promise.success(Seq(42))
+
+    updater ! "wow"
+    updater ! "such"
+    updater ! "publish"
+
+    withDelay {
+      assertEquals(value, Seq(42, 3, 4, 7))
+      assertEquals(result(as.future), Seq(42, 3, 4, 7))
+    }
+
+    as.unsubscribeAll()
+
+    withDelay {
+      // still holds to the last computed value after unsubscribing
+      assertEquals(value, Seq(42, 3, 4, 7))
+      assertEquals(result(as.future), Seq(42, 3, 4, 7))
+    }
+    // triggers reload
+    updater ! "publisher"
+
+    withDelay {
+      // still the old value
+      assertEquals(value, Seq(42, 3, 4, 7))
+      // a new value after reload
+      assertEquals(result(as.future), Seq(42, 9))
+    }
+
+    promise = Promise[Seq[Int]]()
+    as.foreach { value = _ }
+
+    withDelay {
+      assertEquals(value, Seq(42, 3, 4, 7))
+      assertEquals(result(as.future), Seq(42, 9))
+    }
+
+    updater ! "much amaze"
+
+    withDelay {
+      assertEquals(value, Seq(42, 9, 10))
+      assertEquals(result(as.future), Seq(42, 9, 10))
+    }
+
+    promise.success(Seq(42, 3, 4, 7, 9))
+
+    withDelay {
+      assertEquals(value, Seq(42, 3, 4, 7, 9, 10))
+      assertEquals(result(as.future), Seq(42, 3, 4, 7, 9, 10))
+    }
+
+    updater ! "much"
+    updater ! "amaze"
+
+    withDelay {
+      assertEquals(value, Seq(42, 3, 4, 7, 9, 10, 4, 5))
+      assertEquals(result(as.future), Seq(42, 3, 4, 7, 9, 10, 4, 5))
+    }
+  }
 }
